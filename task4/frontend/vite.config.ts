b477866c6b018ff77c1440a0dfc5b9c051d4358f
sanuarkhan220 <sanuarkhan220@gmail.com>
2025-06-01 06:28:12 +0600

    feat: comment out server proxy configuration in Vite setup

--- task4/frontend/vite.config.ts
+++ task4/frontend/vite.config.ts
@@ -4,15 +4,15 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target:
-          process.env.NODE_ENV === "production"
-            ? "https://usermangementb.sanuarkhan220.workers.dev"
-            : "http://localhost:8080",
-        changeOrigin: true,
-      },
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     "/api": {
+  //       target:
+  //         process.env.NODE_ENV === "production"
+  //           ? "https://usermangementb.sanuarkhan220.workers.dev"
+  //           : "http://localhost:8080",
+  //       changeOrigin: true,
+  //     },
+  //   },
+  // },
 });
