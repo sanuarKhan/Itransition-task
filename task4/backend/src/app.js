55b06e1399d9865741e29ac261ab26e9784f20a5
sanuarkhan220 <sanuarkhan220@gmail.com>
2025-06-01 13:23:04 +0600

    fix: update CORS middleware to use default settings

--- task4/backend/src/app.js
+++ task4/backend/src/app.js
@@ -6,7 +6,7 @@ const userRoutes = require("./routes/user.routes");
 const app = express();
 
 // middleware
-app.use(cors("*"));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
